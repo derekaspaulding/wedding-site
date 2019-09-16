22b0462c0714b2537b6ba6bd559437ec5ff22c65
Derek Spaulding <derek@derekaspaulding.com>
2019-09-15 20:25:50 -0500

    add registry links

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,6 +21,23 @@ export default () => (
         </div>
         <h2 className={styles.heading}>Where?</h2>
         <Map />
+
+        <div>
+          <h2 className={styles.heading}>Registry</h2>
+          <div className={styles.registryList}>
+            <span className={styles.registryItem}>
+              <a href="http://tgt.gifts/SpauldingsSealTheDeal" target="_blank">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Target_logo.svg/432px-Target_logo.svg.png" alt="Target" />
+              </a>
+            </span>
+
+            <span className={styles.registryItem}>
+              <a href="https://www.amazon.com/wedding/share/SpauldingsSealTheDeal" target="_blank">
+                <img src="http://g-ec2.images-amazon.com/images/G/01/social/api-share/amazon_logo_500500._V323939215_.png" alt="Target" />
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
     )}
   </Layout>
